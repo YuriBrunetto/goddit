3fe7b2b58309d04c77a93b530c39c728dfc30842
Yuri Brunetto <brunettoyuri@gmail.com>
2024-04-02 14:53:49 -0300

    time conversion

--- main.go
+++ main.go
@@ -3,30 +3,45 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
-func main() {
-	posts, _ := fetchSubredditPosts()
-
-	for _, post := range posts.Data.Children {
-		fmt.Println(post.Data.Title)
-	}
-}
-
-func fetchSubredditPosts() (*SubredditResponse, error) {
+// fetchSubredditPosts retrieves subreddit hot gosts.
+func fetchSubredditPosts(subreddit string) (*SubredditResponse, error) {
 	s := &http.Client{Timeout: time.Second * 8}
-	resp, err := s.Get("https://www.reddit.com/r/diablo4/hot.json")
+	resp, err := s.Get(fmt.Sprintf("https://www.reddit.com/r/%s/hot.json", subreddit))
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("Failed to fetch subreddit posts: %v", err)
 	}
 	defer resp.Body.Close()
 
 	posts := SubredditResponse{}
 	if err := json.NewDecoder(resp.Body).Decode(&posts); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("Failed to decode JSON response: %v", err)
 	}
 
 	return &SubredditResponse{Data: posts.Data}, nil
+}
+
+func main() {
+	posts, err := fetchSubredditPosts("diablo4")
+	if err != nil {
+		fmt.Printf("Error fetching subreddit posts: %v\n", err)
+		os.Exit(1)
+	}
+
+	for _, post := range posts.Data.Children {
+		parts := strings.Split(post.Data.Created.String(), ".")
+		timestamp, err := strconv.ParseInt(parts[0], 10, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		t := time.Unix(timestamp, 0)
+		fmt.Printf("%s - %v\n", post.Data.Title, t)
+	}
 }
--- types.go
+++ types.go
@@ -1,20 +1,23 @@
 package main
 
+import "encoding/json"
+
 type Children struct {
 	Data ChildData `json:"data"`
 }
 
 type ChildData struct {
-	Title                 string `json:"title"`
-	SubredditNamePrefixed string `json:"subreddit_name_prefixed"`
-	Name                  string `json:"name"`
-	LinkFlairText         string `json:"link_flair_text"`
-	LinkFlairBgColor      string `json:"link_flair_background_color"`
-	Thumbnail             string `json:"thumbnail"`
-	Permalink             string `json:"permalink"`
-	URL                   string `json:"url"`
-	Selftext              string `json:"selftext,omitempty"`
-	Author                string `json:"author,omitempty"`
+	Title                 string      `json:"title"`
+	SubredditNamePrefixed string      `json:"subreddit_name_prefixed"`
+	Name                  string      `json:"name"`
+	LinkFlairText         string      `json:"link_flair_text"`
+	LinkFlairBgColor      string      `json:"link_flair_background_color"`
+	Thumbnail             string      `json:"thumbnail"`
+	Permalink             string      `json:"permalink"`
+	URL                   string      `json:"url"`
+	Selftext              string      `json:"selftext,omitempty"`
+	Author                string      `json:"author"`
+	Created               json.Number `json:"created"`
 }
 
 type SubredditResponse struct {
